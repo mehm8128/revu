bb032cda99a261dddfd50a791c5655a6dc30146c
mehm8128 <mehm8128@gmail.com>
2024-04-25 23:36:45 +0900

    線引いたところにレビューコメントポップアップを召喚できるように

--- src/app/[articleId]/review/_components/ReviewPopup.tsx
+++ src/app/[articleId]/review/_components/ReviewPopup.tsx
@@ -1,7 +1,9 @@
+import type { Coordinate } from '@/app/[articleId]/review/page'
 import FieldContainer from '@/components/FieldContainer'
 import Tiptap from '@/features/tiptap/Tiptap'
 import { valibotResolver } from '@hookform/resolvers/valibot'
 import { Box } from '@kuma-ui/core'
+import type { MouseEvent } from 'react'
 import { Controller, useForm } from 'react-hook-form'
 import { minLength, object, string } from 'valibot'
 
@@ -12,7 +14,9 @@ interface Review {
 	comment: string
 }
 
-export default function ReviewPopup() {
+export default function ReviewPopup({
+	coordinate
+}: { coordinate: Coordinate }) {
 	const {
 		formState: { errors },
 		control,
@@ -32,7 +36,18 @@ export default function ReviewPopup() {
 	}
 
 	return (
-		<Box position="absolute">
+		<Box
+			position="absolute"
+			backgroundColor="white"
+			border="1px solid black"
+			borderRadius={12}
+			padding={12}
+			width={240}
+			height={140}
+			left={coordinate.x}
+			top={coordinate.y}
+			onMouseUp={(e: MouseEvent<HTMLDivElement>) => e.stopPropagation()}
+		>
 			<FieldContainer label="レビューコメント" error={errors.comment}>
 				<Controller
 					control={control}
--- src/app/[articleId]/review/page.tsx
+++ src/app/[articleId]/review/page.tsx
@@ -4,8 +4,13 @@ import { useArticle } from '@/features/article/apis/fetchArticle'
 import { parseId } from '@/features/article/model/type'
 import { useReviewList } from '@/features/review/apis/fetchReviewList'
 import { Box, VStack } from '@kuma-ui/core'
-import { useEffect, useState } from 'react'
+import { type MouseEvent, useEffect, useRef, useState } from 'react'
 import ReviewPopup from './_components/ReviewPopup'
+
+export interface Coordinate {
+	x: number
+	y: number
+}
 
 export default function Page({
 	params: { articleId }
@@ -13,11 +18,28 @@ export default function Page({
 	const { data: article } = useArticle(parseId(articleId))
 	const { data: reviews } = useReviewList(parseId(articleId))
 
+	const [selectedText, setSelectedText] = useState('')
+	const [coordinate, setCoordinate] = useState<Coordinate>({ x: 0, y: 0 })
 	const [pendingReviews, setPendingReviews] = useState<string[]>([])
+	const ref = useRef<HTMLDivElement | null>(null)
 
 	const onSubmit = async () => {
 		console.log('保存')
 		// まとめてpostするのかそれぞれpostするのか要検討
+	}
+
+	const handleMouseupEvent = (e: MouseEvent<HTMLDivElement>) => {
+		if (ref.current === null) {
+			return
+		}
+
+		const x = e.clientX
+		const y = e.clientY
+
+		const boxX = ref.current.getBoundingClientRect().x
+		const boxY = ref.current.getBoundingClientRect().y
+
+		setCoordinate({ x: x - boxX, y: y - boxY })
 	}
 
 	useEffect(() => {
@@ -25,6 +47,7 @@ export default function Page({
 			const selected = document.getSelection()
 			if (selected !== null) {
 				console.log(selected.toString())
+				setSelectedText(selected.toString())
 			}
 		}
 
@@ -39,9 +62,9 @@ export default function Page({
 			<h1>{article.title}</h1>
 			<VStack as="form" gap={20} alignItems="center" onSubmit={onSubmit}>
 				<p>{article.description}</p>
-				<Box position="relative">
+				<Box position="relative" onMouseUp={handleMouseupEvent} ref={ref}>
 					<p>{article.content}</p>
-					<ReviewPopup />
+					<ReviewPopup coordinate={coordinate} />
 				</Box>
 				<button type="submit">保存</button>
 			</VStack>
