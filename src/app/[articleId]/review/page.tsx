1501e891ab3af574f14bc8cf318097a3a7731124
mehm8128 <mehm8128@gmail.com>
2024-04-25 22:51:13 +0900

    selectedTextを取れるように

--- src/app/[articleId]/review/page.tsx
+++ src/app/[articleId]/review/page.tsx
@@ -1,20 +1,36 @@
 'use client'
-
 import { useArticle } from '@/features/article/apis/fetchArticle'
 import { parseId } from '@/features/article/model/type'
 import { useReviewList } from '@/features/review/apis/fetchReviewList'
 import { VStack } from '@kuma-ui/core'
+import { useEffect, useState } from 'react'
 
 export default function Page({
-	parmas: { articleId }
-}: { parmas: { articleId: string } }) {
+	params: { articleId }
+}: { params: { articleId: string } }) {
 	const { data: article } = useArticle(parseId(articleId))
 	const { data: reviews } = useReviewList(parseId(articleId))
+
+	const [pendingReviews, setPendingReviews] = useState<string[]>([])
 
 	const onSubmit = async () => {
 		console.log('保存')
 		// まとめてpostするのかそれぞれpostするのか要検討
 	}
+
+	useEffect(() => {
+		const callback = () => {
+			const selected = document.getSelection()
+			if (selected !== null) {
+				console.log(selected.toString())
+			}
+		}
+
+		document.addEventListener('selectionchange', callback)
+		return () => {
+			document.removeEventListener('selectionchange', callback)
+		}
+	}, [])
 
 	return (
 		<main>
